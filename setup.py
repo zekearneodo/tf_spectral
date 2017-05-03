c808cfc0017544d3ebbb511eda7a68e088d8ae19
Zeke Arneodo <zeke.arneodo@gmail.com>
2017-05-02 21:21:55 -0700

    initial commit with function for thresholded, logarithmic spectrogram

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='tf_spectral',
+      version='0.1',
+      description='Spectrogram tools using tensorflow',
+      url='http://github.com/zekearneodo/tf_spectral',
+      author='Zeke Arneodo',
+      author_email='earneodo@ucsd.edu',
+      license='GNU3',
+      packages=['tf_spectral'],
+      install_requires=['numpy', 'tensorflow'],
+      zip_safe=False)
